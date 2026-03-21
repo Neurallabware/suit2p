726158a10b792a76651e24281f9b62443a4ae35b
qian_qichen <qian_qichen@126.com>
2026-03-21 12:09:14 +0000

    add: dynamic alias so suite2p could be clone as submodule rather than installed when integrated with other pipeline

--- suite2p/__init__.py
+++ suite2p/__init__.py
@@ -1,6 +1,12 @@
 """
 Copyright © 2023 Howard Hughes Medical Institute, Authored by Carsen Stringer and Marius Pachitariu.
 """
+import sys
+
+# When vendored under SOMMv2, map the canonical top-level name to this package
+# so internal imports like "from suite2p.xxx import ..." continue to work.
+sys.modules.setdefault("suite2p", sys.modules[__name__])
+
 from suite2p.version import version, version_str
 from suite2p.parameters import default_settings, user_settings, SETTINGS_FOLDER, default_db
 from suite2p.pipeline_s2p import pipeline
